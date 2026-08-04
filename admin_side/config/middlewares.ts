840970669dfa6a75a20af64101bd3ad8edab79b0
NAILLAHHHH <n.ineza1@alustudent.com>
2026-08-04 16:03:45 +0200

    Refactor middleware configuration to use environment variables

--- admin_side/config/middlewares.ts
+++ admin_side/config/middlewares.ts
@@ -1,26 +1,56 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-        process.env.FRONTEND_URL,
-      ].filter(Boolean),
-      headers: ['Content-Type', 'Authorization'],
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default ({ env }: Core.Config.Shared.ConfigParams) => {
+  const useCloudinary = Boolean(env('CLOUDINARY_NAME'));
+  const securityMiddleware = useCloudinary
+    ? {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                'res.cloudinary.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                'res.cloudinary.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+      }
+    : 'strapi::security';
 
-export default config;
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    securityMiddleware,
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: [
+          'http://localhost:3000',
+          'http://127.0.0.1:3000',
+          process.env.FRONTEND_URL,
+        ].filter(Boolean),
+        headers: ['Content-Type', 'Authorization'],
+      },
+    },
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
